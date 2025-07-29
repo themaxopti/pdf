375410d044ad768c09bdd69b17728f054a3a88cf
maksymTverdokhlibRedrocket <mtverdokhlib@redrocket.software>
2025-07-29 17:14:32 +0300

    Made second section

--- index.js
+++ index.js
@@ -6,14 +6,115 @@ const path = require('path');
 (async () => {
     // Параметры
     const styles = fs.readFileSync('styles.css', 'utf-8');
+
+    const content = {
+        firstSection: {
+            items: [
+                {
+                    slug: 'data-card',
+                    heading: 'Основные характеристики',
+                    items: [
+                        {
+                            label: 'VIN-номер',
+                            value: 'Volkswagen'
+                        },
+                        {
+                            label: 'VIN-номер 2',
+                            value: 'Volkswagen'
+                        }
+                    ]
+                },
+                {
+                    slug: 'data-color-card',
+                    heading: 'Цвет автомобиля',
+                    items: [
+                        {
+                            label: 'Белый',
+                            color: '#FEFEFE'
+                        },
+                        {
+                            label: 'Красный',
+                            color: '#FE546D'
+                        }
+                    ]
+                },
+                {
+                    slug: 'data-card-comment-border',
+                    heading: 'Цвет автомобиля',
+                    comment: 'Комментарий'
+                },
+                {
+                    slug: 'data-card-additional-checkboxes',
+                    heading: 'Состояние автомобиля',
+                    items: [
+                        {
+                            label: 'Белый',
+                            value: 'Тест'
+                        },
+                        {
+                            label: 'Красный',
+                            value: 'Тест'
+                        }
+                    ],
+                    labels: [
+                        {
+                            label: 'Белый',
+                            checked: true
+                        },
+                        {
+                            label: 'Красный',
+                            checked: false
+                        }
+                    ]
+                }
+            ]
+        },
+        secondSection: {
+            items: [
+                {
+                    slug: 'data-check-card',
+                    title: 'Уровень масла ДВС',
+                    state: 'error',
+                    comment: {
+                        title: 'Комментарий',
+                        desc: 'Комментарий очень  от.'
+                    },
+                    additionalData: {
+                        labels: [
+                            {
+                                label: 'Белый',
+                                checked: true
+                            },
+                            {
+                                label: 'Красный',
+                                checked: false
+                            }
+                        ]
+                    },
+                    imageUrl: 'https://i.postimg.cc/Rh5Jd1VL/770f63bd3ec58dca14d9c0e9253175d07007ef01.jpg',
+                },
+                {
+                    slug: 'data-check-card',
+                    title: 'Уровень масла ДВС',
+                    state: 'success',
+                    comment: null,
+                    additionalData: null,
+                    imageUrl: 'https://i.postimg.cc/Rh5Jd1VL/770f63bd3ec58dca14d9c0e9253175d07007ef01.jpg',
+                }
+            ]
+        }
+    }
+
+
     const data = {
         name: 'Иван Иванов',
         styles,
+        data: content
     };
 
 
     // Рендерим HTML из EJS-шаблона
-    const templatePath = path.join(__dirname, 'template.ejs');
+    const templatePath = path.join(__dirname, 'views/template.ejs');
     const html = await ejs.renderFile(templatePath, data);
 
     // Создаем PDF через Puppeteer
--- server.js
+++ server.js
@@ -7,8 +7,105 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    const data = {
+        firstSection: {
+            items: [
+                {
+                    slug: 'data-card',
+                    heading: 'Основные характеристики',
+                    items: [
+                        {
+                            label: 'VIN-номер',
+                            value: 'Volkswagen'
+                        },
+                        {
+                            label: 'VIN-номер 2',
+                            value: 'Volkswagen'
+                        }
+                    ]
+                },
+                {
+                    slug: 'data-color-card',
+                    heading: 'Цвет автомобиля',
+                    items: [
+                        {
+                            label: 'Белый',
+                            color: '#FEFEFE'
+                        },
+                        {
+                            label: 'Красный',
+                            color: '#FE546D'
+                        }
+                    ]
+                },
+                {
+                    slug: 'data-card-comment-border',
+                    heading: 'Цвет автомобиля',
+                    comment: 'Комментарий'
+                },
+                {
+                    slug: 'data-card-additional-checkboxes',
+                    heading: 'Состояние автомобиля',
+                    items: [
+                        {
+                            label: 'Белый',
+                            value: 'Тест'
+                        },
+                        {
+                            label: 'Красный',
+                            value: 'Тест'
+                        }
+                    ],
+                    labels: [
+                        {
+                            label: 'Белый',
+                            checked: true
+                        },
+                        {
+                            label: 'Красный',
+                            checked: false
+                        }
+                    ]
+                }
+            ]
+        },
+        secondSection: {
+            items: [
+                {
+                    slug: 'data-check-card',
+                    title: 'Уровень масла ДВС',
+                    state: 'error',
+                    comment: {
+                        title: 'Комментарий',
+                        desc: 'Комментарий очень  от.'
+                    },
+                    additionalData: {
+                        labels: [
+                            {
+                                label: 'Белый',
+                                checked: true
+                            },
+                            {
+                                label: 'Красный',
+                                checked: false
+                            }
+                        ]
+                    },
+                    imageUrl: 'https://i.postimg.cc/Rh5Jd1VL/770f63bd3ec58dca14d9c0e9253175d07007ef01.jpg',
+                },
+                {
+                    slug: 'data-check-card',
+                    title: 'Уровень масла ДВС',
+                    state: 'success',
+                    comment: null,
+                    additionalData: null,
+                    imageUrl: 'https://i.postimg.cc/Rh5Jd1VL/770f63bd3ec58dca14d9c0e9253175d07007ef01.jpg',
+                }
+            ]
+        }
+    }
     const styles = fs.readFileSync('styles.css', 'utf-8');
-    res.render('template', { styles });
+    res.render('template', { styles, data });
 });
 
 app.listen(3000, () => {
